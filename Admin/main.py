b1930555f7d35fa3533565f548f1722f77819016
Manish <anitasharma4440@gmail.com>
2022-02-14 14:28:51 +0530

    icon added

--- Admin/main.py
+++ Admin/main.py
@@ -8,6 +8,7 @@ root.title("Attendence Manager")
 # 1366 x 768
 root.geometry("1080x650")
 root.resizable(False, False)
+root.iconbitmap("Assets/icon.ico")
 
 if Connection().check_database():
     LoginPage(root).draw()
