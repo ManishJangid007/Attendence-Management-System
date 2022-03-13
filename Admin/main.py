fcac1c6e28fbc44adf243cd7ccd7484265cd4da7
Manish <anitasharma4440@gmail.com>
2022-03-13 22:20:50 +0530

    Splash Screen Implemented

--- Admin/main.py
+++ Admin/main.py
@@ -2,6 +2,7 @@ from setupPages.SetupPage import SetupPage
 from tkinter import *
 from LoginPage import LoginPage
 from ServerSide.Connection import *
+from SplashScreen import SplashScreen
 
 root = Tk()
 root.title("Attendence Manager")
@@ -10,9 +11,16 @@ root.geometry("1080x650")
 root.resizable(False, False)
 root.iconbitmap("Assets/icon.ico")
 
-if Connection().check_database():
-    LoginPage(root).draw()
-else:
-    SetupPage(root).draw()
+Sp = SplashScreen(root)
+Sp.draw()
+
+def start():
+    if Connection().check_database():
+        LoginPage(root).draw()
+    else:
+        SetupPage(root).draw()
+    Sp.destroy()
+
+root.after(2000, start)
 
 root.mainloop()
