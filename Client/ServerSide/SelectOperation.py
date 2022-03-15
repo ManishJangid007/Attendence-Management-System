09a15199969bbc5a9a6d580541e3d1d9a792a161
Rahul <rahul@gmail.com>
2022-03-15 09:27:14 +0530

    Client: added main file

--- Client/ServerSide/SelectOperation.py
+++ Client/ServerSide/SelectOperation.py
@@ -1,11 +1,11 @@
 from ServerSide.Connection import Connection
 
+
 class SelectOperation():
     def __init__(self):
-        obj = Connection
+        obj = Connection()
         self.conn = obj.connect()
         self.cur = self.conn.cursor()
-
 
     def verifyTeacher(self, user_name, password):
         try:
@@ -19,3 +19,25 @@ class SelectOperation():
                 return False
         except:
             pass
+
+    def getTeacherId(self, t_user_name):
+        try:
+            query = "SELECT teacher_id FROM Teachers WHERE user_name = %s"
+            value = [t_user_name]
+            self.cur.execute(query, value)
+            result = self.cur.fetchone()
+            return result[0]
+        except:
+            pass
+
+    def getSubjectAccordingToTeacher(self, t_user_name):
+        try:
+            #subject name course_name year
+            teacher_id = self.getTeacherId(t_user_name)
+            query = "SELECT subject_id, course_id, year FROM Subjects WHERE teacher_id = %s"
+            value = [teacher_id]
+            self.cur.execute(query, value)
+            result = self.cur.fetchall()
+            print(result)
+        except Exception as e:
+            print(e)
---
+++ Client/ServerSide/main.py
@@ -0,0 +1,4 @@
+from ServerSide.SelectOperation import SelectOperation
+
+select = SelectOperation()
+select.getSubjectAccordingToTeacher("savita")
